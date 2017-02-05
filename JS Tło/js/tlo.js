d1d9c6486ef151a9145829c23878281b40ef43c2
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2017-02-05 15:46:37 +0100

    zadanie js tlo

---
+++ JS Tło/js/tlo.js
@@ -0,0 +1,8 @@
+'use strict';
+var kolorPpierwszego = document.getElementsByTagName("p")[0];
+var kolorPdrugiego = document.getElementsByTagName("p")[1];
+
+function ustaw_tlo() {
+	kolorPpierwszego.style.backgroundColor = "red";
+	kolorPdrugiego.style.backgroundColor = "green";
+}
